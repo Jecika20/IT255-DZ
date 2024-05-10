aa7d3a403926625633f20a5c8c32cda22cfb7a37
Jecika20 <jelena.radenkov@gmail.com>
2024-05-10 21:14:39 +0200

    IT255 DZ 10-14

--- methotels/src/app/services/room.service.ts
+++ methotels/src/app/services/room.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Room } from '../models/room.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+// Servis sa pogledima (observables)
 @Injectable({
   providedIn: 'root',
 })
 export class RoomService {
-  rooms: Room[] = [];
+  public apiUrl = 'http://localhost:3000';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  addRoom(room: Room) {
-    this.rooms.push(room);
+  addRoom(room: Room): Observable<Room> {
+    return this.http.post<Room>(`${this.apiUrl}/rooms`, room, httpOptions);
   }
 
-  getRooms(): Room[] {
-    return this.rooms;
+  updateRoom(room: Room): Observable<Room> {
+    return this.http.put<Room>(
+      `${this.apiUrl}/rooms/${room.id}`,
+      room,
+      httpOptions
+    );
+  }
+
+  getRooms(): Observable<Room[]> {
+    return this.http.get<Room[]>(`${this.apiUrl}/rooms`);
+  }
+
+  deleteRoom(id: string): Observable<Room> {
+    return this.http.delete<Room>(`${this.apiUrl}/rooms/${id}`);
   }
 
   getTotalPrice(quantity: number, price: number) {
